f6ee28d7562c87aa9a8702260a5fcb5773f355fb
Ibrahim Hamed <ibrahimhamed2016@hotmail.com>
2018-10-27 18:30:37 +0200

    Last Modified Angular 6

--- src/app/addskill/addskill.component.ts
+++ src/app/addskill/addskill.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddskillComponent implements OnInit {
 
+  // name:string     = "إبراهيم حامد"
+  // phone:string    = "01008928060"
+  // skill:string    = "مهندس برمجيات"
+  // province:string = "القاهرة"
+  // price:string    = "200 جنيه"
+  // comments:string = "مرحبا بكم فى معرض مهارتي الخاصة. اهلا وسهلا بكم يرجي التواصل معي على رقم الهاتف لمزيد من التفاصيل."
+
+  data = {
+    name      : "إبراهيم حامد",
+    phone     : "01008928060",
+    skill     : "مهندس برمجيات",
+    province  : "القاهرة",
+    price     : "200 جنيه",
+    comments  :"مرحبا بكم فى معرض مهارتي الخاصة. اهلا وسهلا بكم يرجي التواصل معي على رقم الهاتف لمزيد من التفاصيل."
+  }
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.data.name);
   }
 
 }
